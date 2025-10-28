f0995a7d19c34f867449ecc6ff2ef5c08d812bc3
susananzth <susananzth.dev@gmail.com>
2025-10-28 20:15:02 +0000

    feat(router, layout): Añadiendo router dom y layour

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Post from "./pages/Post";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {/* Layout como ruta "padre" */}
+      <Route path="/" element={<Layout />}>
+        {/* Redirección: si van a la raíz o a otra página */}
+        <Route index element={<Navigate to="/posts" replace />} />
+
+        <Route path="posts" element={<Post />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
