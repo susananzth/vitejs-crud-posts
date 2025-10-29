1569879cd75b78efa30d2afc6199c2f5cb2d037b
susananzth <susananzth.dev@gmail.com>
2025-10-29 22:07:01 +0000

    fix(main): Agregando subdirectorio para githubpages

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const BASE_PATH = "/vitejs-crud-posts";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_PATH}>
       <App />
     </BrowserRouter>
   </StrictMode>
